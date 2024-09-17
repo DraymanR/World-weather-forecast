596615281e1c3f51bb492e97f5be216918e561c8
DraymanR <dr0548424205@gmail.com>
2024-09-18 00:21:12 +0300

    happy ending all is well

--- src/GetTemperatureInformation.js
+++ src/GetTemperatureInformation.js
@@ -9,10 +9,7 @@ function GetTemperatureInformation({ city, title }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}
-            &appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}
-            &lang=${process.env.REACT_APP_OPENWEATHERMAP_LANGUAGE}
-            &units=${process.env.REACT_APP_OPENWEATHERMAP_UNITS_OF_MEASUREMENT}`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&lang=${process.env.REACT_APP_OPENWEATHERMAP_LANGUAGE}&units=${process.env.REACT_APP_OPENWEATHERMAP_UNITS_OF_MEASUREMENT}`);
         setData(response.data);
         setError(null); // Clear previous errors if request is successful
       } catch (error) {
@@ -26,33 +23,33 @@ function GetTemperatureInformation({ city, title }) {
 
   const getFilteredData = () => {
     if (!data) return null;
-    const { main, weather } = data;
+    const { main, weather, name } = data;
     const temperature = main?.temp;
     const feels_like = main?.feels_like;
     const humidity = main?.humidity;
     const description = weather?.[0]?.description;
-
-    return { temperature, feels_like, humidity, description };
+    const city = name;
+    return { temperature, feels_like, humidity, description, city };
   };
 
   const filteredData = getFilteredData();
 
   return (
     <div className="card">
-      <h2 className="card-title">{title}</h2>
       {error ? (
         <p>{error}</p>
       ) : (
         filteredData ? (
           <div>
+            <h2 className="card-title">{filteredData.city}</h2>
             <div className="weather-icon">
               {filteredData.feels_like <= 20 ? '☁️' :
                 filteredData.feels_like >= 30 ? '☀️' : '⛅'}
             </div>
-            <p className="temperature">Temperature: {filteredData.temperature} °C</p>
-            <p className="temperature">Feels Like: {filteredData.feels_like} °C</p>
-            <p>Humidity: {filteredData.humidity} %</p>
-            <p className="description">Description: {filteredData.description}</p>
+            <p className="temperature">טמפרטורה נמדדת: {filteredData.temperature} °C</p>
+            <p className="temperature">טמפרטורה מורגשת: {filteredData.feels_like} °C</p>
+            <p>אחוזי לחות: % {filteredData.humidity}</p>
+            <p className="description">תיאור: {filteredData.description}</p>
           </div>
         ) : (
           '...טוען '
